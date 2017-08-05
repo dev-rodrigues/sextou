4640c77189f7a9f49b29a741437145309a403995
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-08-05 14:58:37 -0300

    Remove rails

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-require 'rspotify/oauth'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'],ENV['SPOTIFY_SECRET_ID'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
-end
