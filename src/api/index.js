93522d0012a40eda1556e6a8d57dd00e9ea261af
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-08-05 17:08:38 -0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,15 @@
 const koa = require('koa'),
       koaRouter = require('koa-router'),
       koaBody = require('koa-bodyparser'),
-      { graphqlKoa, graphiqlKoa } = require('apollo-server-koa');
+      { graphqlKoa, graphiqlKoa } = require('apollo-server-koa'),
+      spotifySchema = require('./schemas/spotify');
 
 const app = new koa();
 const router = new koaRouter();
 const PORT = 3000;
 
-router.post('/graphql', koaBody(), graphqlKoa({ schema: myGraphQLSchema }));
-router.get('/graphql', graphqlKoa({ schema: myGraphQLSchema }));
+router.post('/graphql', koaBody(), graphqlKoa({ schema: spotifySchema }));
+router.get('/graphql', graphqlKoa({ schema: spotifySchema }));
 
 router.post('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
 router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
@@ -16,6 +17,6 @@ router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`eae sextou? 😌 🎧`);
 });
